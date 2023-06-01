36704eddf5d45fca1333e28182d5002e7d7f5cc4
Victor Avila <vavilamo@cisco.com>
2023-05-31 22:28:35 -0700

    feat: add globals to preview

--- libs/ui/.storybook/preview.ts
+++ libs/ui/.storybook/preview.ts
@@ -1,1 +1,20 @@
+import { Preview } from '@storybook/angular';
 
+const preview: Preview = {
+  // All stories expect a theme arg
+  argTypes: {
+    variant: {
+      control: 'select',
+      options: ['default', 'destructive', 'outline', 'secondary', 'ghost'],
+    },
+    size: { control: 'select', options: ['default', 'sm', 'lg'] },
+  },
+
+  // The default value of the theme arg to all stories
+  args: {
+    variant: 'default',
+    size: 'default',
+  },
+};
+
+export default preview;
