bc7e648f85951c55cda30990403024aef2a071ea
Victor Avila <vavilamo@cisco.com>
2023-05-31 23:57:38 -0700

    refactor: renaming stuff

--- libs/ui/src/lib/button/button.component.stories.ts
+++ libs/ui/src/lib/button/button.component.stories.ts
@@ -2,11 +2,11 @@ import { Meta, StoryObj } from '@storybook/angular';
 import { ButtonComponent } from './button.component';
 
 export default {
-  title: 'ButtonComponent',
+  title: 'Button',
   component: ButtonComponent,
 } as Meta<ButtonComponent>;
 
-export const Button: StoryObj<ButtonComponent> = {
+export const Default: StoryObj<ButtonComponent> = {
   render: (args: ButtonComponent) => ({
     props: args,
     template: `<button slick-btn variant="${args.variant}" size=${args.size}>Button</button>`,
