a694343735832fb6adbe3c0ade88cb7e85b508d7
Victor Avila <vavilamo@cisco.com>
2023-06-05 16:43:31 -0700

    feat: remove global args

--- libs/ui/.storybook/preview.ts
+++ libs/ui/.storybook/preview.ts
@@ -4,33 +4,6 @@ const preview: Preview = {
   parameters: {
     layout: 'centered',
   },
-  // All stories expect a theme arg
-  argTypes: {
-    variant: {
-      control: 'select',
-      description:
-        'The variant controls how the element looks and potentially performs',
-      options: [
-        'default',
-        'destructive',
-        'outline',
-        'secondary',
-        'ghost',
-        'link',
-      ],
-    },
-    size: {
-      control: 'select',
-      description: 'The size of the element',
-      options: ['default', 'sm', 'lg'],
-    },
-  },
-
-  // The default value of the theme arg to all stories
-  args: {
-    variant: 'default',
-    size: 'default',
-  },
 };
 
 export default preview;
